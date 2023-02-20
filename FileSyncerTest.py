09f442f93fefd03407ceee73a2d94412ab3d776f
Josh Grainge <77454059+JoshGrainge@users.noreply.github.com>
2023-02-20 18:30:58 -0500

    Changed load files function to work recursively for sub directories

--- FileSyncerTest.py
+++ FileSyncerTest.py
@@ -1,6 +1,7 @@
 from tkinter.filedialog import askopenfilenames
 from tkinter.filedialog import askdirectory
 from pcloud import PyCloud
+import os
 
 
 def print_save_sub_directories():
@@ -35,7 +36,6 @@ def load_game_files(gameDirectory):
     savedFolderPath = ""
 
     for dir in saveDir['metadata']['contents']:
-        currPath = dir["path"]
         if dir['path'] == '/Saves/'+ gameDirectory:
             savedFolderPath = dir['path']
             folderId = dir['folderid']
@@ -49,32 +49,46 @@ def load_game_files(gameDirectory):
     # TODO can recursively call this function if its a folder to load all files in the folder (would be a good 
     # thing to call it open all files or something where the root dir would be the passed as the folders name)
     files = pc.listfolder(folderid=folderId)
+    load_all_files_in_directory(files, savedFolderPath, _savefilesDestination)
+    print_finish_message("Finished Downloading files")
+   
+
+# Recursive function to load all files in a folder
+# Loads all files in folder (does this recursively for all sub directories as well)
+def load_all_files_in_directory(files, cloudFolderPath, localPath):
     for file in files['metadata']['contents']:
-        _fd = pc.file_open(path=savedFolderPath + "/" + file['name'], flags=0)
+        # Create folder with folder name and load all files in folder into newly created folder
+        if(file['isfolder']):
+            # if folder in local directory already exists, populate that instead
+            newCloudPath = cloudFolderPath + '/' + file['name']
+            newFolderPath = localPath + '/' + file['name']
+            if os.path.exists(newFolderPath) == False:
+                os.mkdir(newFolderPath)
+            folderFiles = pc.listfolder(folderid=file['folderid'])
+            load_all_files_in_directory(folderFiles, newCloudPath, newFolderPath)
+            continue
+            
+        _fd = pc.file_open(path=cloudFolderPath + "/" + file['name'], flags=0)
         stats = pc.stat(fileid=file['fileid'])
         size = stats['metadata']['size']
         bytes = pc.file_read(fd=_fd['fd'], count=size)
 
-        binaryFile = open(_savefilesDestination+"/"+file['name'], 'wb')
+        binaryFile = open(localPath+"/"+file['name'], 'wb')
         binaryFile.write(bytes)
         binaryFile.close()
 
-        print(file['name'] + " was downloaded to: " + _savefilesDestination)
+        print(file['name'] + " was downloaded to: " + localPath)
 
         pc.file_close(fd=_fd['fd'])
-
-    print_finish_message("Finished Downloading files")
-
-def loadAllFilesInDirectory(_folderId):
-    return
-
 
 def print_finish_message(message):
     print(message)
     input("Press enter to return to menu...")
 
+# TODO make this a user input to login with a checkbox of remember me to have them only have to do that once
 email = input("Please enter email:\n")
-password = input("Please enter password:\n")
+password = input("Please enter email:\n")
+
 pc = PyCloud(email, password)
 pc.listfolder(folderid=0)
 
@@ -89,7 +103,5 @@ gameName = input("Enter game directory you would like to use (Without the slash)
 # clicks the button it will open proper dialogue box with proper action
 if uploadingFiles == "upload":
     save_game_files(gameName)
-
 else:
     load_game_files(gameName)
-
