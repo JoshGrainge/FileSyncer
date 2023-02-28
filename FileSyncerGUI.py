f7cafb7eb5fdac1a6f3330d10f6b5313d76d7f6c
Josh Grainge <77454059+JoshGrainge@users.noreply.github.com>
2023-02-27 21:52:28 -0500

    Combined GUI and backend functionality

--- FileSyncerGUI.py
+++ FileSyncerGUI.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from FileSyncerTest import * 
 
 global login
 login = False
@@ -8,8 +9,13 @@ savedLoginPass = ""
 
 savedTheme = ""
 
-# Load current directories
-currentDirectories = ['Create New Directory', 'Combo 2', 'Combo 3']
+currentDirectories = []
+
+# values to be kept after main window refreshes
+global uploadCombo
+uploadCombo = ""
+global downloadCombo
+downloadCombo = ""
 
 
 def load_login_info():
@@ -19,13 +25,12 @@ def load_login_info():
     savedLoginEmail = "Saved Email"
     savedLoginPass = "Saved Password"
 
-
 def open_login_modal():
     loginLayout = [
         [sg.Text("pCloud Email:")],
-        [sg.Input()],
+        [sg.Input("josh.grainge@hotmail.com")],
         [sg.Text("pCloud Password:")],
-        [sg.Input()],
+        [sg.Input("ThisIsASecretPasswordForMeAndMyStinkyFriendsOnly!")],
         [sg.Text("Remember login:"),sg.Checkbox('')],
         [sg.Button("Login")]
     ]
@@ -33,56 +38,69 @@ def open_login_modal():
     loginWindow = sg.Window("File Syncer", loginLayout, modal=True, element_justification='c')
     while True:
         event, values = loginWindow.read()
-        print(event, values)
 
         # Check if login is correct
         if event == "Login":
             global login
             login = True
 
-            # Save login info when login was successful
-            if values[2] == True:
-                print("Should remember login")
+            if attempt_login(values[0], values[1]):
+                # Save login info when login was successful
+                if values[2] == True:
+                    print("Should remember login")
 
-            break
+                break
 
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
 
     loginWindow.close()
 
+while login == False:
+        open_login_modal()
+
+# Load current directories
+currentDirectories = print_save_sub_directories()
+
 def create_main_window():
+    global uploadCombo
+    global downloadCombo
+
     mainLayout = [
     [sg.Text("File Syncer")],
     
-    [sg.TabGroup([[sg.Tab('Upload',[[sg.Text("Cloud Directories:"),sg.Combo(values=currentDirectories, readonly=True, enable_events=True, k='CLOUD_DIR'), sg.Button("Upload Files")], 
+    [sg.TabGroup([[sg.Tab('Upload',[[sg.Text("Cloud Directories:"),sg.Combo(values=['Create New Directory'] + currentDirectories, default_value=uploadCombo, readonly=True, enable_events=True, k='CLOUD_DIR_UPLOAD'), sg.Button("Upload Files")], 
                                     [sg.Text("Enter new game directory", k="NEW_DIR_TITLE"), sg.Input("", disabled=True, k="ADD_DIR_INPUT")]]), 
-                   sg.Tab('Download', [[sg.Button("Download Files")]])
+                   sg.Tab('Download', [[sg.Text("Cloud Directories:"),sg.Combo(values=currentDirectories, default_value=downloadCombo, readonly=True, enable_events=True, k='CLOUD_DIR_DOWNLOAD'),sg.Button("Download Files")]])
                 ]])
     ],
     [sg.Text("")],
     [sg.Text("")],
-    [sg.Text("Theme:"), sg.Combo(values=sg.theme_list(), readonly=True, enable_events=True, k='THEME')]
+    [sg.Text("Theme:"), sg.Combo(values=sg.theme_list(), default_value=sg.theme(), readonly=True, enable_events=True, k='THEME')]
     ]
     return sg.Window("FileSyncer", mainLayout, element_justification='c')
 
 mainWindow = create_main_window()
 
-while login == False:
-        open_login_modal()
-
 
 while True:
     event, values = mainWindow.read()
-    print(event, values)
+    mainWindow['ADD_DIR_INPUT'].update(disabled=values['CLOUD_DIR_UPLOAD'] != "Create New Directory")
+
+    uploadCombo = values['CLOUD_DIR_UPLOAD']
+    downloadCombo = values['CLOUD_DIR_DOWNLOAD']
 
     if event == "Upload Files":
-        print("Upload Files Stuff")
-
-    mainWindow['ADD_DIR_INPUT'].update(disabled=values['CLOUD_DIR'] != "Create New Directory")
+        result = combine_paths(values['CLOUD_DIR_UPLOAD'], values['ADD_DIR_INPUT'])
+        upload_save_game_files(result)
+        # Update directories when new directory is added
+        if values['CLOUD_DIR_UPLOAD'] == "Create New Directory":
+            currentDirectories = print_save_sub_directories()
+            mainWindow.close()
+            mainWindow = create_main_window()
 
     if event == "Download Files":
-        print("Download Files Stuff")
+        download_save_game_files(values['CLOUD_DIR_DOWNLOAD'])
 
     if event == sg.WIN_CLOSED or event == "My simple app.":
         break
--- FileSyncerTest.py
+++ FileSyncerTest.py
@@ -5,31 +5,68 @@ import os
 # TODO Auto create Saves folder in root
 # TODO Alter print sub directories to return array of directories as strings
 # TODO Create function to ensure login was successful
+# TODO Handle file browser being canceled out of because it crashes program right now
+
+global pc
+
+def attempt_login(_email, _password):
+    global pc
+
+    # Login details not specified
+    if len(_email) == 0 or len(_password) == 0:
+        return False
+    
+    try:
+        pc = PyCloud(_email, _password)
+        pc.listfolder(folderid=0)
+        return True
+    except:
+        return False
+    
 
 def print_save_sub_directories():
+    global pc
     meta = pc.listfolder(folderid=0)
     saveDir = pc.listfolder(path=meta['metadata']['contents'][0]['path'])
     print("Current game save directories are: ")
+
+    directories = []
+
     # List all current sub directories (AKA current games with save files)
     # Removes the directory slash because non programmers are using this mainly
     for dir in saveDir['metadata']['contents']:
         trimmedDirectory = dir['path'].replace(saveDir['metadata']["path"] + "/", '')
+        directories.append(trimmedDirectory)
         print(trimmedDirectory)
 
+    return directories
+        
+def combine_paths(cloudDir, addDirInput):
+    if(cloudDir == "Create New Directory"):
+        result = addDirInput
+    else:
+        result = cloudDir
+
+    return result
+
 def upload_save_game_files(gameDirectory):
+    global pc
+
     localDir = askdirectory()
 
     # Create folder in cloud if none exists
     cloudDir = '/Saves/' + gameDirectory
     pc.createfolderifnotexists(path=cloudDir)
 
-    upload_all_files_in_local_dir(localDir, cloudDir)
+    _upload_all_files_in_local_dir(localDir, cloudDir)
 
     print_finish_message("Finished uploading files")
 
 
 # Uploads all files recursively to the cloud
-def upload_all_files_in_local_dir(localDir, cloudDir):
+def _upload_all_files_in_local_dir(localDir, cloudDir):
+    global pc
+
     files = os.listdir(localDir)
     for file in files:
         filePath = os.path.join(localDir, file)
@@ -37,12 +74,14 @@ def upload_all_files_in_local_dir(localDir, cloudDir):
             print("Creating folder: " + file + " in cloud at dir: " + cloudDir)
             newCloudDir = cloudDir + '/' + file
             pc.createfolderifnotexists(path=newCloudDir)
-            upload_all_files_in_local_dir(filePath, newCloudDir)
+            _upload_all_files_in_local_dir(filePath, newCloudDir)
             continue
         print("Creating file: " + file + " in cloud at: " + cloudDir)
         pc.uploadfile(files=[os.path.join(localDir, file)], path=cloudDir)
 
 def download_save_game_files(gameDirectory):
+    global pc
+
     _savefilesDestination = askdirectory()
 
     meta = pc.listfolder(folderid=0)
@@ -64,13 +103,15 @@ def download_save_game_files(gameDirectory):
         return
 
     files = pc.listfolder(folderid=folderId)
-    download_all_files_in_directory(files, savedFolderPath, _savefilesDestination)
+    _download_all_files_in_directory(files, savedFolderPath, _savefilesDestination)
     print_finish_message("Finished Downloading files")
    
 
 # Recursive function to load all files in a folder
 # Loads all files in folder (does this recursively for all sub directories as well)
-def download_all_files_in_directory(files, cloudFolderPath, localPath):
+def _download_all_files_in_directory(files, cloudFolderPath, localPath):
+    global pc
+
     for file in files['metadata']['contents']:
         # Create folder with folder name and load all files in folder into newly created folder
         if(file['isfolder']):
@@ -80,7 +121,7 @@ def download_all_files_in_directory(files, cloudFolderPath, localPath):
             if os.path.exists(newFolderPath) == False:
                 os.mkdir(newFolderPath)
             folderFiles = pc.listfolder(folderid=file['folderid'])
-            download_all_files_in_directory(folderFiles, newCloudPath, newFolderPath)
+            _download_all_files_in_directory(folderFiles, newCloudPath, newFolderPath)
             continue
             
         # Write cloud file to local file
@@ -99,8 +140,8 @@ def download_all_files_in_directory(files, cloudFolderPath, localPath):
 
 def print_finish_message(message):
     print(message)
-    input("Press enter to return to menu...")
 
+    '''
 # TODO make this a user input to login with a checkbox of remember me to have them only have to do that once
 email = input("Enter pCloud email: \n")
 password = input("Enter pCloud password: \n")
@@ -121,3 +162,4 @@ if uploadingFiles == "upload":
     upload_save_game_files(gameName)
 else:
     download_save_game_files(gameName)
+'''
